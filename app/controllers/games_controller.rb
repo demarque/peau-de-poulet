fcabc43bba11059bf41ed5a6cd1a56810ff1ae21
Sebastien Rosa <sebastien@demarque.com>
2012-12-07 15:59:41 -0500

    Small stuff

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -10,18 +10,18 @@ class GamesController < ApplicationController
 
   def create
     game = Game.new params[:game]
-    game.cheater = (params[:game][:score].to_i > 75)
+    game.cheater = (params[:game][:score].to_i > 275)
     game.save
 
     session[:name] = params[:game][:name]
 
     if Rails.env == 'production'
       if game.cheater
-        message = "Bien essayé #{name}, mais les tricheurs ne sont pas admis!"
-        %x(/usr/local/bin/yammer -g 1111719 -u "#{message}")
+        message = "Bien essayé #{game.name}, mais les tricheurs ne sont pas admis!"
+        %x(/usr/local/bin/yammo "#{message}")
       elsif Game.count == 0 or game.score >= Game.where(:cheater.ne => true).desc(:score).limit(1).first.score
         message = "#{game.name} à établi un nouveau record! La marque à battre est #{game.score}."
-        %x(/usr/local/bin/yammer -g 1111719 -u "#{game}")
+        %x(/usr/local/bin/yammo "#{message}")
       end
     end
 
@@ -32,9 +32,3 @@ class GamesController < ApplicationController
     end
   end
 end
-
-# Fit the ipad screens
-# Enlarge tap zone
-# Improve performance
-# Add sounds
-# Security. Identify cheaters
