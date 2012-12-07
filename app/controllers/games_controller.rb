d31c575eda59c4c295fad9fe8db19a4d1f07e952
Sebastien Rosa <sebastien@demarque.com>
2012-12-07 13:10:31 -0500

    Push on yammer

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 class GamesController < ApplicationController
   def index
     @game = Game.new
@@ -8,10 +10,20 @@ class GamesController < ApplicationController
 
   def create
     game = Game.new params[:game]
-    game.cheater = true if params[:game][:score].to_i > 75
+    game.cheater = (params[:game][:score].to_i > 75)
     game.save
 
     session[:name] = params[:game][:name]
+
+    if Rails.env == 'production'
+      if game.cheater
+        message = "Bien essayé #{name}, mais les tricheurs ne sont pas admis!"
+        %x(/usr/local/bin/yammer -g 1111719 -u "#{message}")
+      elsif Game.count == 0 or game.score >= Game.where(:cheater.ne => true).desc(:score).limit(1).first.score
+        message = "#{game.name} à établi un nouveau record! La marque à battre est #{game.score}."
+        %x(/usr/local/bin/yammer -g 1111719 -u "#{game}")
+      end
+    end
 
     if game.cheater
       redirect_to 'http://chickenonaraft.com/'
