7fd025aff46ddb8b57651da92404c000880b4ec3
Sebastien Rosa <sebastien@demarque.com>
2012-12-15 19:23:46 -0500

    Patch a security hole

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -15,16 +15,6 @@ class GamesController < ApplicationController
 
     session[:name] = params[:game][:name]
 
-    if Rails.env == 'production'
-      if game.cheater
-        message = "Bien essayé #{game.name}, mais les tricheurs ne sont pas admis!"
-        %x(/usr/local/bin/yammo "#{message}")
-      elsif Game.count == 0 or game.score >= Game.where(:cheater.ne => true).desc(:score).limit(1).first.score
-        message = "#{game.name} à établi un nouveau record! La marque à battre est #{game.score}."
-        %x(/usr/local/bin/yammo "#{message}")
-      end
-    end
-
     if game.cheater
       redirect_to 'http://chickenonaraft.com/'
     else
